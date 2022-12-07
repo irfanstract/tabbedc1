660a9654ee0cca3a18c1596384878aa01054b54b
meh <meh@meh>
2022-12-07 17:32:59 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ctabs2207',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
