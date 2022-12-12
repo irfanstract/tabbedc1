2bd870d85daf033ebcf72364b1ed49841b75e1f4
meh <meh@meh>
2022-12-12 16:49:58 +0700

    library - ionic-react-components

---
+++ src/library/useIonicUi1.tsx
@@ -0,0 +1,44 @@
+
+
+
+
+
+export { 
+  IonIcon as IonIcon ,
+  
+  IonLabel as Label,
+  
+  // IonRouterOutlet,
+  // IonTabBar,
+  // IonTabButton,
+
+  IonTabs    as Tabs ,
+  IonPage    as Page ,
+  IonTitle   as Title ,
+  IonContent as Content ,
+  IonHeader  as Header ,
+  IonToolbar as Toolbar , 
+  
+  useIonToast       as useToast ,
+  useIonAlert       as useAlert ,
+  useIonActionSheet as useActionSheet ,
+  useIonModal       as useModality ,
+
+  IonReorderGroup as ReorderGroup ,
+  IonList         as List ,
+  IonItemSliding  as ItemSliding ,
+  IonItemDivider  as ItemDivider ,
+  IonItem         as Item ,
+
+  IonCard         as Card ,
+  IonCardContent  as CardContent ,
+  IonButton       as Button ,
+  IonReorder      as Reorder ,
+  IonInput        as Input , 
+} from '@ionic/react';
+export {
+  IonApp as IonApp,  
+} from '@ionic/react';
+;;;{};
+
+
