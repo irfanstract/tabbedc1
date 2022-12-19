be0017542fcaa68b54fc79ea275fb892716906e4
meh <meh@meh>
2022-12-19 08:52:16 +0700

    React-based core stuffs

--- src/library/useExistingComponentPropTypes.tsx
+++ src/library/useExistingComponentPropTypes.tsx
@@ -18,10 +18,10 @@ export {} ; // TS-1208
 
 type ComponentProps<A> = (
   never 
-  | [A] extends [{     (a: infer Props) : unknown ; }                 ] ? (Props & object) : never
-  | [A] extends [{ new (a: infer Props) : unknown ; }                 ] ? (Props & object) : never
-  | [A] extends [React.Component<infer Props, infer State, infer SS  >] ? (Props & object) : never
-  | [A] extends [React.ExoticComponent<infer Props >                  ] ? (Props & object) : never
+  | A extends {     (a: infer Props) : unknown ; }                  ? (Props & object) : never
+  | A extends { new (a: infer Props) : unknown ; }                  ? (Props & object) : never
+  | A extends React.Component<infer Props, infer State, infer SS  > ? (Props & object) : never
+  | A extends React.ExoticComponent<infer Props >                   ? (Props & object) : never
 ) ;
 namespace ComponentProps { ; } // TS-1205
 
