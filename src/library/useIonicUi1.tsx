806007d9e39686ab69112a2386d31e020c54228f
meh <meh@meh>
2022-12-19 08:55:20 +0700

    Ionic

--- src/library/useIonicUi1.tsx
+++ src/library/useIonicUi1.tsx
@@ -4,38 +4,55 @@
 
 
 export { 
-  IonIcon as IonIcon ,
-  
-  IonLabel as Label,
-  
-  // IonRouterOutlet,
-  // IonTabBar,
-  // IonTabButton,
+  /** HOOKS - IONIC POPUPS */ 
 
-  IonTabs    as Tabs ,
-  IonPage    as Page ,
-  IonTitle   as Title ,
-  IonContent as Content ,
-  IonHeader  as Header ,
-  IonToolbar as Toolbar , 
-  
   useIonToast       as useToast ,
   useIonAlert       as useAlert ,
   useIonActionSheet as useActionSheet ,
   useIonModal       as useModality ,
 
-  IonReorderGroup as ReorderGroup ,
-  IonList         as List ,
-  IonItemSliding  as ItemSliding ,
-  IonItemDivider  as ItemDivider ,
-  IonItem         as Item ,
-
-  IonCard         as Card ,
-  IonCardContent  as CardContent ,
-  IonButton       as Button ,
-  IonReorder      as Reorder ,
+  /* STANDALONE COMPONENTS */
+  
+  IonIcon   as IonIcon ,
+  
+  IonTabs         as Tabs    ,
+  IonCard         as Card    ,
+  IonToolbar      as Toolbar , 
+  IonButton       as Button  ,
+  IonList         as List    ,
+  IonSelect       as WhichOnePicker   ,
+  IonSelectOption as WhichOneOption   ,
+  IonToggle       as Toggle   ,
+  IonCheckbox     as CheckBox   ,
+  IonRadio        as RadioButton ,
+  IonRadioGroup   as RadioButtonGroup ,
+  IonReorder      as Reorder   ,
+  IonContent      as Content   ,
   IonInput        as Input , 
+  IonRange        as BoundedRangeSlider ,
+  IonModal        as IonModal ,
+  // AlertButton     as AlertButton ,
+
+  /* PART/SLOT(s) */
+
+  IonPage         as Page    , // top-level ; not applicable alsewhere
+  IonTitle        as Title    , // ION-PAGE
+  IonCardContent  as CardContent ,
+  IonHeader       as Header  ,
+  IonRouterOutlet ,
+  
+  IonLabel as Label, // assumes enclosing ListItem
+  
+  IonTabBar    , // assumes enclosing Tabs
+  IonTabButton , // assumes enclosing Tabs
+
+  IonReorderGroup as ReorderGroup , // assumes enclosing List
+  IonItemSliding  as ItemSliding , // assumes enclosing List
+  IonItemDivider  as ItemDivider , // assumes enclosing List
+  IonItem         as Item , // assumes enclosing List
+
 } from '@ionic/react';
+export * as iconNames from 'ionicons/icons';
 export {
   IonApp as IonApp,  
 } from '@ionic/react';
