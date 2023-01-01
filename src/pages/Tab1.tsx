b5ee1f8c42555a0bb44bb60c88bd1c53467de97b
meh <meh@meh>
2023-01-01 15:58:55 +0700

    Pages - Tab 1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -2,8 +2,30 @@
 
 /* eslint-disable import/first, @typescript-eslint/no-unused-vars    */
 
+/** core lang */
+import SS from "lodash" ;
+import Immutable from "immutable";
+import * as Collections from "library/util-immutable-datastructure" ;
+import * as Math2 from "library/util-math-max" ;
+
+/** React */
+import React from 'react';
+import * as RCS from "library/rcs1" ;
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import * as Ion from "library/useIonicUi1" ;
+
+/** Components */
 import ExploreContainer from '../components/ExploreContainer';
+import CurrentDateTime1 from 'components/CurrentDateTime1';
+import UebR from 'components/UebR';
+import { DccDemo, DccDemoTwo1, } from 'components/DraftEditorCc';
+import { SecondsOfTimeDurativeSlider, SdlsdDemo, } from 'components/SecondsOfTimeDurationSlider';
+import { OosDemo, } from 'components/OverlayOrSequencer/f1';
+import { CmC, } from 'components/Tl1';
+import { xpxs, } from "components/PrimeLogo";
+import fsmt from "components/MetroFshExpoBanner";
+
+/** CSS Imports */
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -20,7 +42,24 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <div 
+        style={{
+          display: "flex" ,
+          flexDirection: "column",
+        }}
+        >
+          { xpxs }
+          { fsmt }
+        </div>
         <ExploreContainer name="Tab 1 page" />
+        <CurrentDateTime1 />
+        { null && (
+        <React.Fragment key={1}>
+        <DccDemoTwo1 />
+        </React.Fragment>
+        ) }
+        <SdlsdDemo />
+        <CmC />
       </IonContent>
     </IonPage>
   );
