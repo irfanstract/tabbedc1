e178df275f26ec766126cda1a0705bf16b9ed110
meh <meh@meh>
2023-01-05 10:52:05 +0700

    submodule UsrFileOpen organisative fix

--- src/components/usrFileOpen.tsx
+++ src/components/usrFileOpen.tsx
@@ -1,5 +1,18 @@
 
 
+/* eslint-disable import/first, @typescript-eslint/no-unused-vars    */
+
+import SS from "lodash" ;
+import Immutable from "immutable";
+import * as Collections from "library/util-immutable-datastructure" ;
+import * as Math2 from "library/util-math-max" ;
+import React from 'react';
+import * as RCS from "library/rcs1" ;
+import { SvgWithIncl, } from "library/rcsSvg1";
+import * as RCSSVG from "library/rcsSvg1";
+import CurrentDateTime1 from 'components/CurrentDateTime1';
+import { CmC, } from 'components/Tl1';  
+import * as Ion from "library/useIonicUi1" ;
 
 
 
@@ -8,56 +21,89 @@
 
 
 
-export default (
-  async (...[config = { allowMultiple: true, ifCancelled: "leave-hanging", } ] : [
-    config ?: { 
-      allowMultiple : boolean ;
-      ifCancelled : "leave-hanging" | "return-zero-length" ;
-    } ,
-  ] ) => {
-    ;
-    const {
-      allowMultiple: shallAllowMoreThanOne,
-    } = config ;
-    ;
+
+
+
+
+
+import usrFileOpen from "library/usrFileOpen";
+/**   
+ * 
+ * @deprecated
+ */
+export default usrFileOpen ;
+export const FileOpenBtnC = (
+  SS.identity<(
+    React.FC<(
+      {}
+      & { onInput : false | React.Dispatch<{ submittedFiles: File[] ; }> ; }
+      & Partial<{ allowMultipleItems : boolean ; }>
+    )>
+  )>(function FileOpenBtnCIMpl({
+    onInput: highLevelOnInput ,
+    allowMultipleItems = true ,
+  }) {
     return (
-      await new Promise<File[]>(resolve => {
-        ;
-        const fb = (
-          document.createElement("input")
-        ) ;
-        /**   
-         * weren't this explicitly set, this'd default to something else
-         */
-        fb.type = "file" ;
-        /**   
-         * by default only one selection, but
-         * we want it to be {@link shallAllowMoreThanOne }
-         */
-        fb.multiple = (
-          shallAllowMoreThanOne
-        ) ;
-        /**   
-         * add the relevant listener.
-         */
-        fb.addEventListener("change", () => {
-          const f0 = (
-            (() => {
-              const files = fb.files ;
-              return files ? Array.from(files ) : null ;
-            } )()
+      <Ion.Button 
+      children={"open..." }
+      color={"secondary"}
+      {...(() : RCS.ComponentProps<typeof Ion.Button> => {
+        if (highLevelOnInput) {
+          const onClick = (
+            async ( ) => {
+              const submittedFiles = (
+                await (
+                  usrFileOpen({
+                    allowMultiple: (
+                      allowMultipleItems
+                    ) ,
+                    ifCancelled: (
+                      "leave-hanging"
+                    ) ,
+                  })
+                ) 
+              ) ;
+              highLevelOnInput({
+                submittedFiles: submittedFiles ,
+              }) ;
+            }
           ) ;
-          if (f0 ) {
-            resolve(f0) ;
-          } else {
-            //
-          }
-        } ) ;
-        /**   
-         * make it truly showing.
-         */
-        fb.click() ;
-      } ) 
+          return {
+            onClick: onClick ,
+          } ;
+        } else {
+          return {
+            disabled: true ,
+          } ;
+        }
+      } )() }
+      />
+    ) ;
+  } )
+) ;
+export const FileOpenBtnDemoC = (
+  function () {
+    const [f, setF] = (
+      React.useState<File[] >([])
+    ) ;
+    return (
+      <div>
+        <p>
+          opened files : {}
+          <code>{ f.length }</code> {}
+        </p>
+        <p>
+          <label>
+            To Open File(s) : {}
+            <FileOpenBtnC 
+            allowMultipleItems
+            onInput={({ submittedFiles, }) => {
+              setF(submittedFiles ) ;
+            } }
+            />
+          </label>
+        </p>
+      </div>
     ) ;
   }
 ) ;
