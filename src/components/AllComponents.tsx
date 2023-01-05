20b6bbb2c4c2fad55501ede7290c7e9beff5f89d
meh <meh@meh>
2023-01-05 10:52:29 +0700

    AllComponents

---
+++ src/components/AllComponents.tsx
@@ -0,0 +1,19 @@
+
+
+/* eslint-disable import/first, @typescript-eslint/no-unused-vars    */
+
+ 
+export {} ; // TS-1208
+
+
+
+
+
+
+
+export * from "components/usrFileOpen" ;
+export { default as CurrentDateTime1, } from "components/CurrentDateTime1"; 
+export * from "library/useIonicUi1" ;
+
+
+
