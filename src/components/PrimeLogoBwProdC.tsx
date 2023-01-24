d9f5e26160da956bc285797c858892b67a76dee9
meh <meh@meh>
2023-01-24 09:06:52 +0700

    rename-and-amend `pattern1`

--- src/components/PrimeLogoBwProdC.tsx
+++ src/components/PrimeLogoBwProdC.tsx
@@ -206,11 +206,11 @@ const xpxs = (
       ) ,
     }) ;
     const { 
-      pattern1,
-    } = xcm({
+      pattern1C,
+    } = ({
       
-      pattern1 : (
-        withAttachment(({ assignedId }) => (
+      pattern1C : (
+        withAttachmentC(({ assignedId }) => (
           <g
           id={assignedId }
           style={{  
@@ -295,7 +295,7 @@ const xpxs = (
           mask={(
             // `url(${pattern1[0].href }P)` 
             // `url(${simpleCircleDef1[0].href }P)` 
-            `url(${pattern1[0].href }P)` 
+            `url(${pattern1C.href }P)` 
           )}
           >
             <rect
@@ -316,7 +316,6 @@ const xpxs = (
         </g>
       </g>
       <>
-      { (pattern1        )[1] }
       { (rect3           )[1] }
       </>
       </>
