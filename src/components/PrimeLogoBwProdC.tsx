3e01ceda2064f378960757e98ae56209d0391186
meh <meh@meh>
2023-01-24 08:56:38 +0700

    amend the imports

--- src/components/PrimeLogoBwProdC.tsx
+++ src/components/PrimeLogoBwProdC.tsx
@@ -37,7 +37,19 @@ const xpxs = (
     ...{ } ,
   }}
   > 
-  { ({ withAttachment, withAttachmentAll1 = null, componentId: componentId, }) => (
+  { ({ 
+    
+    //
+    withAttachment, 
+    withAttachmentAll1 = null, 
+    //
+    withAttachmentC ,
+    asClippedC ,
+    
+    //
+    componentId: componentId, 
+
+  }) => (
   <React.Fragment>
   { (
     /**   
