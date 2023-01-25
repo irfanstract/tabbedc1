f8864f9c4b025fd4a68cb31de249380ab772aef8
meh <meh@meh>
2023-01-25 07:58:03 +0700

    fix the organisation

--- src/library/audioctx/makeBassDrumKickSoundCd.tsx
+++ src/library/audioctx/makeBassDrumKickSoundCd.tsx
@@ -27,131 +27,14 @@ export {} ;
 
 
 
-type CtxAndDest = { 
-  c: BaseAudioContext ; 
-  d: AudioNode | AudioParam ; 
-} ;
-const CtxAndDest = {} ;
-type  CtxTSpecifier           = { tInCtx      : number      ; } ; 
-const CtxTSpecifier           = {  } ; 
-type  AmpFactorSpecifier      = { ampF        : number      ; } ;
-const AmpFactorSpecifier      = {  } ; 
-type  DurationalSpecifier     = { duration    : number      ; } ; 
-const DurationalSpecifier     = {  } ; 
-let defaultBassDropAmpFac: number = (
-  2 ** -2
-) ;
+import { CtxAndDest             , } from "library/audioctx/makeBassDropSoundCd";
+import { CtxTSpecifier          , } from "library/audioctx/makeBassDropSoundCd";
+import { AmpFactorSpecifier     , } from "library/audioctx/makeBassDropSoundCd";
+import { DurationalSpecifier    , } from "library/audioctx/makeBassDropSoundCd";
+import { makeBassDrumKickSound , } from "library/audioctx/makeBassDropSoundCd";
 export default (
-  SS.identity<{
-    (ctx: CtxAndDest ) : void ;
-  }>(function implMakeBassDrumKickSound({
-    c,
-    d,
-  }) {
-    ;
-    return (
-      makeBassDropSound({ 
-        c, d, 
-      } , { 
-        duration: 0.5 , 
-        ampF: (2 ** 0.625) * defaultBassDropAmpFac , 
-        finalFreq: 45 ,
-        startFreq: 65 ,
-      } )
-    ) ;
-  } )
+  makeBassDrumKickSound
 ) ;
-export const makeBassDropSound = (
-  SS.identity<{
-    (...args : [
-      ctx: (
-        {}
-        & CtxAndDest
-        & Partial<CtxTSpecifier>
-      ) ,
-      dp ?: (
-        {}
-        & Partial<AmpFactorSpecifier>
-        & Partial<DurationalSpecifier>
-        & EitherBothSetOrBothUnset<{ finalFreq: number ; startFreq: number ; }>
-      ) ,
-    ] ) : void ;
-  }>(function implMakeBassDropSound({
-    c,
-    d,
-    tInCtx: t1 = c.currentTime ,
-  } , {
-    duration = 4 ,
-    ampF = defaultBassDropAmpFac ,
-    finalFreq = 55 ,
-    startFreq = (2 ** (6/12 ) ) * finalFreq ,
-  } = {} ) {
-    ;
-    const amp1 = (
-      new (class extends GainNode {
-        constructor() {
-          super(c) ;
-          this.gain.setValueAtTime(ampF, 0, ) ;
-        }
-      } )
-    ) ;
-    const fade11 = (
-      new (class extends GainNode {
-        constructor() {
-          super(c) ;
-          this.gain.setValueAtTime(1, 0, ) ;
-          {
-            {
-              (
-                this.gain
-                .setValueAtTime(1, t1, )
-              ) ;
-              (
-                this.gain
-                .linearRampToValueAtTime(0, t1 + duration, )
-              ) ;
-            }
-          }
-        }
-      } )
-    ) ;
-    const o2 = (
-      new (class extends OscillatorNode {
-        constructor() {
-          super(c) ;
-          (
-            this.frequency
-            .setValueAtTime(finalFreq, 0, )
-          ) ;
-          (
-            this.detune
-            .setValueAtTime(new (class { semitones !: number ; constructor(public octaves: number) { this.semitones = octaves * 12 ; } ; } )(Math.log2(startFreq / finalFreq ) ).semitones * 100 , 0, )
-          ) ;
-          if (1) {
-            {
-              (
-                this.detune
-                .setValueAtTime(new (class { semitones !: number ; constructor(public octaves: number) { this.semitones = octaves * 12 ; } ; } )(Math.log2(startFreq / finalFreq ) ).semitones * 100, t1, )
-              ) ;
-              (
-                this.detune
-                .linearRampToValueAtTime(0, t1 + duration, )
-              ) ;
-            }
-          }
-        }
-      } )
-    ) ;
-    (
-      o2
-      .connect(fade11 )
-      .connect(amp1 )
-      .connect(d )
-    ) ;
-    o2.start(t1 ) ;
-    o2.stop (t1 + (1.5 * duration ) ) ;
-    ;
-  } )
-) ;
+export { makeBassDropSound , } from "library/audioctx/makeBassDropSoundCd";
 
 
