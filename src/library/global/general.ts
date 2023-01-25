bfce3240ad172ec36fd995b753427515026d5cda
meh <meh@meh>
2023-01-25 07:29:56 +0700

    move the def into global

--- src/library/global/general.ts
+++ src/library/global/general.ts
@@ -13,6 +13,70 @@ import Immutable from "immutable";
 
 
 declare global {
+
+   /**   
+    * either 
+    * - all properties set
+    * - all properties unset
+    * 
+    * *to avoid surprising behaviours when using this TD, 
+    * avoid non-`object`s, and {@link Required make all known properties required and non-`undefined` }*.
+    * 
+    */
+   type EitherBothSetOrBothUnset<A extends {} > = (
+      A | { [k in keyof A ] ?: never ; }
+      
+      /**   
+       * every *key* must consistently appear in every *alternative*, for two reasons:
+       * - otherwise, the resulting type will be considered to omit the key
+       * - to become `[key] ?: never ;`, which is the heart of all these
+       */
+   ) ; 
+   /**   
+   * exactly one be set and all others be unset.
+   * 
+   * consider *frequency and period* as example;
+   * there's invariant that `freq * period === 1 `.
+   * to prevent assigning/giving/specifying conflicting values at once, 
+   * it'd be good to restrict the argumentation/constraints to exactly one of them .
+   * 
+   * *to avoid surprising behaviours when using this TD, 
+   * avoid non-`object`s, and {@link Required make all known properties required and non-`undefined` }*.
+   * 
+   */
+   type EitherSetAndOthersUnset<A extends {} > = (
+      /**   
+       * every *key* must consistently appear in every *alternative*, for two reasons:
+       * - otherwise, the resulting type will be considered to omit the key
+       * - to become `[key] ?: never ;`, which is the hart of all these
+       */
+      { 
+         [whichToRequire in keyof A ] : (
+            { [k in whichToRequire ] -?: A[k] ; }
+            &
+            { [k in keyof Omit<A, whichToRequire > ] ?: never ; }
+         ) ;
+   }[keyof A ]
+   ) ; 
+   /**   
+   * either
+   * (a) exactly one set, as in {@link EitherSetAndOthersUnset },
+   * (b) all (left) unset 
+   */
+   type EitherSetOrBothUnset<A extends {} > = (
+      EitherSetAndOthersUnset<A> // obviously - allow cases of exact one property set
+      | 
+      { [k in keyof A ] ?: never ; } // allow cases where none set
+   ) ; 
+   
+   
+   
+   
+   
+   
+} ;
+
+declare global {
    type Exactly<A> = A ; 
    type ExactlyNonNull<A extends {}> = A ; 
 } 
