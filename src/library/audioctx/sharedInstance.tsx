4b6ee74523a60b5de930f5500cba9fafaad46aa4
meh <meh@meh>
2023-02-05 08:53:35 +0700

    drop the RCSSVG imports

--- src/library/audioctx/sharedInstance.tsx
+++ src/library/audioctx/sharedInstance.tsx
@@ -8,8 +8,6 @@ import * as Collections from "library/util-immutable-datastructure" ;
 import * as Math2 from "library/util-math-max" ;
 import React from 'react';
 import * as RCS from "library/rcs1" ;
-import { SvgWithIncl, } from "library/rcsSvg1";
-import * as RCSSVG from "library/rcsSvg1";
 import CurrentDateTime1 from 'components/CurrentDateTime1';
 import * as Ion from "library/useIonicUi1" ;
 
