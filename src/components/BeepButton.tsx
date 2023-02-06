523c33d2e636c0656d29c11b0122ca8654c673f1
meh <meh@meh>
2023-02-06 11:25:07 +0700

    make changes to implement and test a naïve FDS (Frequency-Domain Shifter)

--- src/components/BeepButton.tsx
+++ src/components/BeepButton.tsx
@@ -55,10 +55,18 @@ export default (
             })
           ) ;
           if (1) {
-            nd100.close({ t: c.currentTime + (5 * 60 ) , }) ;
+            nd100.close({ t: c.currentTime + (15 ) , }) ;
           }
           const nd10 = (
             nd100
+            .withConstantAmp(2 ** -2, )
+            .withNativeAudioNodeFlt1({
+              newImplementingNdInstance1: ({ ctx, }) => {
+                return (
+                  new AudioWorkletNode(ctx, "freqsh1" )
+                ) ;
+              } ,
+            })
             .asAmplSyncedPrctWhiteNoise()
             // .withConstantAmp(2 ** -1, )
             .withVariableAmp()
