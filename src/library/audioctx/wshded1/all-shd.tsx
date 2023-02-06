26efb2805bf3e50bfcb668e54f1c3df354742871
meh <meh@meh>
2023-02-06 12:23:28 +0700

    `xStdWorkletScripts`

---
+++ src/library/audioctx/wshded1/all-shd.tsx
@@ -0,0 +1,42 @@
+
+
+/* eslint-disable import/first, @typescript-eslint/no-unused-vars    */
+
+import SS from "lodash" ;
+import Immutable from "immutable";
+import * as Collections from "library/util-immutable-datastructure" ;
+import * as Math2 from "library/util-math-max" ;
+
+
+
+export {} ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+const values = (
+  (
+    <A1 extends object , A2>(obj: A1 & { [key: string]: URL ; } ) => (
+      obj
+    ) 
+  )({
+    // GAINOF : new URL(("shd-gainof.mjs"     ), import.meta.url, ) ,
+  })
+) ;
+export default values ;
+console["log"]({ values, }) ;
+
+
+
+
+
+
